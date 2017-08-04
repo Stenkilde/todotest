391d9518a8314c4a8d1e150123d57293bccc4c3c
Stenkilde <hello@stenkil.de>
2017-08-05 01:10:51 +0200

    Added state, click handler and list

--- src/App.js
+++ src/App.js
@@ -3,13 +3,59 @@ import './App.css';
 
 class App extends Component {
 
-	
+	constructor() {
+		super();
+
+		this.state = {
+			todos: [
+				{
+					id: 1,
+					title: "Make React App",
+					completed: false
+				},
+				{
+					id: 2,
+					title: "Add backend service",
+					completed: false
+				},
+				{
+					id: 3,
+					title: "Be Awesome!",
+					completed: true
+				}
+			]
+		}
+	}
+
+	handleClick(todo) {
+		const newArray = this.state.todos.filter((item) => {
+			return item !== todo;
+		})
+
+		this.setState({
+			todos: newArray
+		})
+	}
 
 	render() {
 		return (
 			<div className="App">
 				<h1>Todo list</h1>
-				
+				<ul className="list">
+					{(() => {
+						return (
+							this.state.todos.map((todo, index) => {
+								return (
+									<li onClick={() => this.handleClick(todo)} 
+										className="item"
+										key={index}>
+										{todo.title}
+									</li>
+								);
+							})
+						);
+					})()}
+				</ul>
 			</div>
 		);
 	}
